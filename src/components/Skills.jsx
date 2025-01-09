8d54e0c019d538f320ae77233fa18e5361262aca
VoHieu906 <vohieutd09@gmail.com>
2025-01-09 15:12:14 +0700

    add review section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -30,6 +30,8 @@ const Skills = () => {
             variants={variants}
             initial="hidden"
             whileInView="visible"
+            whileHover={{ scale: 1.1 }}
+            viewport={{ margin: "50px", once: true }}
             key={i}
             className="flex items-center justify-center gap-x-3 rounded-xl border border-yellow-500 bg-zinc-200 px-5 py-2 lg:px-2"
           >
